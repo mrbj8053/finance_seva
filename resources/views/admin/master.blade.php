7bd6f544f197eef8360a66ef38021c77907531ef
mrbj8053 <www.bhupenderjangra@gmail.com>
2023-07-28 17:13:33 +0530

    set footer credits

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -21,11 +21,11 @@
 
   @yield('mainContent')
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2021 <a href="{{env('APP_WEBSITE')}}">{{env("APP_NAME")}}</a>.</strong>
     All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
+    {{-- <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.2.0
-    </div>
+    </div> --}}
   </footer>
 
   <!-- Control Sidebar -->
