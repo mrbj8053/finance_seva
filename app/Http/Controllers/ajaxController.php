52096dd1697a93b5fe5af4af7c6e22f5f80cfb4a
mrbj8053 <www.bhupenderjangra@gmail.com>
2023-07-31 23:37:23 +0530

    closign done

--- app/Http/Controllers/ajaxController.php
+++ app/Http/Controllers/ajaxController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Income;
+use App\Models\PackageRequest;
 use App\Models\User;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -38,23 +41,38 @@ class ajaxController extends Controller
             {
             $package=$user->packageRequest->packageApplied;
             $roi=$package->entry_amount*($package->roi/100);
+            $day=date("d");
+            $activeDateDay=Carbon::parse($user->PackageRequest->updated_at)->format('d');
+            if($day>=1 && $day<=15)
+            {
+                $diff=15-$activeDateDay;
+            }
+            else
+            {
+                $diff=31-$activeDateDay;
+            }
+            $roi=$roi/30;
+            $roi=$roi*$diff;
+
             $adminCharge=$roi*0.10;
             $netAmount=$roi-$adminCharge;
 
             //send roi income
             $income=new Income();
             $income->user_id=$user->id;
+            $income->from_user=$user->id;
             $income->income_type="ROI";
             $income->amount=$roi;
             $income->admin_charge=$adminCharge;
             $income->admin_charge_per=10;
             $income->net_amount=$netAmount;
-            // $income->save();
+            $income->save();
 
             $level=1;
             $sponsor=$user->sponsor;
             while(!empty($sponsor) && $level<16)
             {
+
                if($sponsor->is_active==1)
                {
                     //now check if two direct active
@@ -63,17 +81,55 @@ class ajaxController extends Controller
                     {
                         if($this->isEligibleForLevel($sponsor->direct_business,$level))
                         {
+                            $levelIncomePer=0;
+                            switch($level)
+                            {
+                                case 1:
+                                $levelIncomePer=0.10;
+                                break;
+                                case 2:
+                                    $levelIncomePer=0.05;
+                                    break;
+                                case 3:
+                                    $levelIncomePer=0.04;
+                                    break;
+                                case 4:
+                                    $levelIncomePer=0.03;
+                                    break;
+                                case 5:
+                                    $levelIncomePer=0.02;
+                                    break;
+                                default:
+                                    $levelIncomePer=0.01;
+                                    break;
 
+                            }
+                            $levelIncome=$roi*$levelIncomePer;
+                            $levelAdminCharge=$levelIncome*0.10;
+                            $netLevelIncome=$levelIncome-$levelAdminCharge;
+                            $insertLevelIncome=new Income();
+                            $insertLevelIncome->user_id=$sponsor->id;
+                            $insertLevelIncome->from_user=$user->id;
+                            $insertLevelIncome->income_type="Level";
+                            $insertLevelIncome->amount=$roi;
+                            $insertLevelIncome->level=$level;
+                            $insertLevelIncome->admin_charge=$levelAdminCharge;
+                            $insertLevelIncome->admin_charge_per=10;
+                            $insertLevelIncome->net_amount=$netLevelIncome;
+                            $insertLevelIncome->save();
+                            $level++;
                         }
-                        else
-                        {
-
-                        }
-                    }
 
                }
-
             }
+               $sponsor=User::where('own_id',$sponsor->sponsor_id)->first();
+               if(empty($sponsor))
+               break;
+            }
+
+
+
+
 
             }
 
@@ -82,25 +138,18 @@ class ajaxController extends Controller
     }
     function isEligibleForLevel($directBusiness,$level)
     {
-        if($directBusiness>=50000 && $level<=5)
-        {
-            return true;
-        }
-        else if($directBusiness>=100000 && $level<=10)
-        {
-            return true;
-        }
-        else if($directBusiness>=300000 && $level<=15)
+        $directBusiness=$directBusiness-10000;
+        $openLevel=$directBusiness/10000;
+
+        if($openLevel>=$level)
         {
             return true;
         }
         else
         {
             return false;
+
         }
     }
-    function insertLevelIncome($toUser,$fromUser,)
-    {
 
-    }
 }
